bd82e770c807857f93532b7912255a1936c33dce
NotNotQuinn <quinn.github@gmail.com>
2022-05-21 02:57:22 +0100

    Randomize /announce emote

--- main.go
+++ main.go
@@ -10,6 +10,11 @@ import (
 	"github.com/gempir/go-twitch-irc/v3"
 )
 
+var (
+	fruit    = []string{"🍇", "🍈", "🍉", "🍊", "🍋", "🍌", "🍍", "🥭", "🍎", "🍏", "🍐", "🍑", "🍒", "🍓", "🥝"}
+	cdEmotes = []string{"💿", "forsenCD", "pepeCD", "pajaCD"}
+)
+
 func main() {
 	var botClient *twitch.Client
 	{ // scoped so variables containing private information arent even availible outside
@@ -19,31 +24,20 @@ func main() {
 	}
 
 	// do stuff
-	botClient.Join("turtoise", "quinndt", "snappingbot", "pajlada", "supinic")
-	// announceSnappingbotGone := func(message twitch.PrivateMessage) {
-	// 	// fmt.Println("anounce")
-	// 	command := strings.ToLower(strings.Split(message.Message, " ")[0])
-	// 	if strings.HasPrefix(message.Message, "+") &&
-	// 		command != "+" &&
-	// 		command != "+l" &&
-	// 		command != "+ratio" {
-	// 		// fmt.Println("anounce2")
-	// 		RegisterUserChannelComboAllin1(message.User.ID, message.Channel)
-	// 		botClient.Say(message.Channel, "snappingbot is offline sorry for any inconvenience - @t󠀀urtoise")
-	// 	}
-	// }
+	rand.Seed(time.Now().Unix())
+	botClient.Join("pajlada", "supinic")
 	reactToPajbot := func(message twitch.PrivateMessage) {
-		if message.User.Name == "pajbot" && message.Action && message.Message == "pajaS 🚨 ALERT" {
+		if message.User.ID == "82008718" && message.Action && message.Message == "pajaS 🚨 ALERT" {
 			UserRegisterLimit("pajbot")
 			botClient.Say(message.Channel, "/me pajaVanish 🚨 ALERT RECEIVED")
 		}
-		if message.User.Name == "mm_sutilitybot" && strings.HasPrefix(message.Message, "/announce 🅱") {
+		if message.User.ID == "442600612" && strings.HasPrefix(message.Message, "/announce ") {
 			UserRegisterLimit("mm_sutilitybot")
-			botClient.Say(message.Channel, "/ /announce 💿")
+			botClient.Say(message.Channel, "/ /announce "+cdEmotes[rand.Intn(len(cdEmotes))])
 		}
 	}
 	reactToSupibot := func(message twitch.PrivateMessage) {
-		if message.User.Name == "supibot" && message.Message == "ppCircle" {
+		if message.User.ID == "68136884" && message.Message == "ppCircle" {
 			UserRegisterLimit("supibot")
 			botClient.Say(message.Channel, "ppOverheat ￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼ ￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼  ppCircleHeat")
 		}
@@ -54,16 +48,12 @@ func main() {
 			fmt.Println("skip because ratelimit")
 			return
 		}
-		if message.Channel == "turtoise" || message.Channel == "snappingbot" || message.Channel == "quinndt" {
-			// qannounceSnappingbotGone(message)
-		} else if message.Channel == "pajlada" {
+		if message.Channel == "pajlada" {
 			reactToPajbot(message)
 		} else if message.Channel == "supinic" {
 			reactToSupibot(message)
 		}
 	})
-	fruit := []string{"🍇", "🍈", "🍉", "🍊", "🍋", "🍌", "🍍", "🥭", "🍎", "🍏", "🍐", "🍑", "🍒", "🍓", "🥝"}
-	rand.Seed(time.Now().Unix())
 	botClient.OnConnect(func() {
 		fmt.Println("Connected bot")
 		botClient.Say("turtoise", "/me turtMunch "+fruit[rand.Intn(len(fruit))])
